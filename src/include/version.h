8b9dec684adbedefb6029f9b89df69e3dc110f53
Service Build Slave <it.devops@lairdconnect.com>
2024-05-22 04:14:18 +0000

    bump to build number 12.0.0.6

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 	#error "error: undefined development version - please define via build system"
 #endif
 
-#define SUMMIT_BUILD_NUMBER "12.0.0.5"
+#define SUMMIT_BUILD_NUMBER "12.0.0.6"
 
 #ifndef SDC_SDK_MSB
 #error "error: API version defines not present"
