b4fb43a4293713c1eea4313956a09b38d59dbacf
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-06-12 09:02:42 -0400

    bump to build number 0.12.0.0

--- src/include/version.h
+++ src/include/version.h
@@ -3,7 +3,7 @@
 	#error "error: undefined development version - please define via build system"
 #endif
 
-#define SUMMIT_BUILD_NUMBER "0.12.0.1"
+#define SUMMIT_BUILD_NUMBER "0.12.0.0"
 
 #ifndef SDC_SDK_MSB
 #error "error: API version defines not present"
